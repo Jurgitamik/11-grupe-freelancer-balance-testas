e304f45b666d88af3852caeca8701f1be351ee77
Jurgitamik <56818575+Jurgitamik@users.noreply.github.com>
2019-12-04 22:27:56 +0200

    table3

--- js/function.js
+++ js/function.js
@@ -1,24 +1,37 @@
 function renderRow ( months, accounts ) {
    let HTML = '';
+//    let balance[j] = accounts[j].income - accounts[j].expense;
+   
+   
    
    for ( let i=0; i<months.length; i++) {
+      
         HTML += `<div class="table-row">
                     <div class="cell">${months[i]}</div>
                 </div>`;
    }
-   document.getElementById('accounts').innerHTML = HTML;
+
 
    for ( let j=0; j<accounts.length;j++) {
+    if ( !accounts[j].income ) {
+        accounts[j].income = '0';
+    }
+    if ( !accounts[j].expense ) {
+        accounts[j].expense = '0';
+    } 
+    if ( !accounts[j].balance ) {
+        accounts[j].balance = '0';
+    }
+    
+           
+ 
         HTML += `<div class="table-row">
                     <div class="cell">${accounts[j].month}</div>
-                    <div class="cell">${accounts[j].income}</div>
-                    <div class="cell">${accounts[j].expense}</div>
-                    <div class="cell">${accounts[j].balance}</div>
+                    <div class="cell">${accounts[j].income + '.00 Eur'}</div>
+                    <div class="cell">${accounts[j].expense + '.00 Eur'}</div>
+                    <div class="cell">${accounts[j].balance + '.00 Eur'}</div>
                 </div>`;
-   }
-   document.getElementById('accounts').innerHTML = HTML;
-
-
-    
-    return;
+   
+    }
+    return document.getElementById('accounts').innerHTML = HTML;
 }
