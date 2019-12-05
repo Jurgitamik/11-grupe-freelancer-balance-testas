051be458a003af54e58432880e5ee11b8249b5da
Jurgitamik <56818575+Jurgitamik@users.noreply.github.com>
2019-12-05 13:56:42 +0200

    table

--- js/function.js
+++ js/function.js
@@ -1,37 +1,30 @@
 function renderRow ( months, accounts ) {
    let HTML = '';
 //    let balance[j] = accounts[j].income - accounts[j].expense;
-   
-   
-   
-   for ( let i=0; i<months.length; i++) {
-      
-        HTML += `<div class="table-row">
-                    <div class="cell">${months[i]}</div>
-                </div>`;
-   }
 
 
-   for ( let j=0; j<accounts.length;j++) {
-    if ( !accounts[j].income ) {
-        accounts[j].income = '0';
+   for ( let i=0; i<accounts.length; i++) {
+    if ( !accounts[i].income ) {
+        accounts[i].income = '0';
     }
-    if ( !accounts[j].expense ) {
-        accounts[j].expense = '0';
+    if ( !accounts[i].expense ) {
+        accounts[i].expense = '0';
     } 
-    if ( !accounts[j].balance ) {
-        accounts[j].balance = '0';
+    if ( !accounts[i].balance ) {
+        accounts[i].balance = '0';
     }
-    
-           
+
+          
  
         HTML += `<div class="table-row">
-                    <div class="cell">${accounts[j].month}</div>
-                    <div class="cell">${accounts[j].income + '.00 Eur'}</div>
-                    <div class="cell">${accounts[j].expense + '.00 Eur'}</div>
-                    <div class="cell">${accounts[j].balance + '.00 Eur'}</div>
+                    <div class="cell">${accounts[i].month}</div>
+                    <div class="cell">${months[i]}</div>
+                    <div class="cell">${accounts[i].income + '.00 Eur'}</div>
+                    <div class="cell">${accounts[i].expense + '.00 Eur'}</div>
+                    <div class="cell">${accounts[i].balance + '.00 Eur'}</div>
                 </div>`;
    
     }
+
     return document.getElementById('accounts').innerHTML = HTML;
 }
